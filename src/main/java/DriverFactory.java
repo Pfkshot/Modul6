06769ade6e0619b240ebeb29f3163b4dec1c88c3
Игорь Васильев <pfkshot@gmail.com>
2024-10-26 01:54:00 +0300

    add DZ5

--- src/main/java/DriverFactory.java
+++ src/main/java/DriverFactory.java
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 
 import static io.appium.java_client.remote.AndroidMobileCapabilityType.APP_ACTIVITY;
 import static io.appium.java_client.remote.AndroidMobileCapabilityType.APP_PACKAGE;
+import static io.appium.java_client.remote.MobileCapabilityType.DEVICE_NAME;
 import static io.appium.java_client.remote.MobileCapabilityType.NO_RESET;
 
 public class DriverFactory {
@@ -15,8 +16,9 @@ public class DriverFactory {
     public AndroidDriver<?> setUp() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "ANDROID");
-        capabilities.setCapability(APP_PACKAGE, "ru-filit-mvideo-b2c.ru");
-        capabilities.setCapability(APP_ACTIVITY, "ui.splash.view.SplashScreen");
+        capabilities.setCapability(DEVICE_NAME,"emulator-5554");
+        capabilities.setCapability(APP_PACKAGE,"com.kaspersky.kaspressample");
+        capabilities.setCapability(APP_ACTIVITY,"com.kaspersky.kaspressample.MainActivity");
         capabilities.setCapability(NO_RESET, true);
 
         URL remoteURL = new URL("http://localhost:4723/wd/hub");
